b80ca54673e505accc4e2c06d41a471deb565daa
Flowers2222 <100409688+Flowers2222@users.noreply.github.com>
2022-04-17 14:54:27 +0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* A=headA;
+        ListNode* B=headB;
+        while(A!=B)
+        {
+            A= A!=nullptr? A->next:headB;
+            B= B!=nullptr? B->next:headA;
+        }
+        return A;
+    }
